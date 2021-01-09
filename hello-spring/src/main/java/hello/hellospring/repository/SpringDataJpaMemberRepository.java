6d7178e9264d325731fdbb1a748143430c9dde05
imnotmoon <imnotmoon@gmail.com>
2021-01-09 23:34:14 +0900

    Add some libraries

--- hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import javax.accessibility.Accessible;
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
